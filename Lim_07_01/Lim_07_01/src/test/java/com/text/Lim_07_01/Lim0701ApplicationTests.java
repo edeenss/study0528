7c65f6e701c22620b1c050c3820f88581016de33
DESKTOP-H6GDJ27\se_im <lse223111@gmail.com>
2025-07-01 15:50:39 +0900

    sdasd

---
+++ Lim_07_01/Lim_07_01/src/test/java/com/text/Lim_07_01/Lim0701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text.Lim_07_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lim0701ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
